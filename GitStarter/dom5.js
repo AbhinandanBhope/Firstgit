fa5a5bf132c08e3fbab4c7c6826dd6683e5dba07
AbhinandanBhope <abhinandanbhope1642000@gmail.com>
2023-01-10 12:48:23 +0530

    newChangesDom

---
+++ GitStarter/dom5.js
@@ -0,0 +1,57 @@
+//console.dir(document);
+//console.log(document.URL);
+//
+//document.title=123;
+//console.log(document.title);
+//console.log(document.doctype);
+//console.log(document.links);
+//console.log(document.images);
+//console.log(document.getElementById('header-title'));
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent ='hellow';
+
+//headerTitle.innerHTML ='Goodbye';
+//console.log(headerTitle);
+//headerTitle.innerHTML='<h3>hellow</h3>';
+//headerTitle.style.borderBottom ="solid 3px #000";
+//headerTitle.style.borderBottom
+
+// GET ELEMENTS BY CLASSNAME//
+//var items = document.getElementsByClassName('list-group-item');
+//items[1].getElementsByClassName.fontWeight='bold';
+//items[1].textContent='Hellow 2';
+//items[2].style.color ='green';
+//for(var i=0;i<items.length;i++){
+   
+  //  items[i].getElementsByClassName.fontWeight='bold';
+//}
+
+//var li = document.getElementsByTagName('li');
+//console.log(li[1]);
+//li[1].getElementsByTagName.fontWeight='bold';
+//li[1].textContent='Hellow 1';
+//li[2].style.color ='yellow';
+//for(var i=0;i<items.length;i++){
+    
+  //  items[i].getElementsByClassName.fontWeight='heavy (900)';
+//}
+var header =document.querySelector('#main-header');
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color='White';
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor ='green';
+var odd =document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
+
+
+
+
+
+
+
+
+
